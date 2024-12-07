66e4716def447be144ee3d654de4e657a83d922d
Harry Xu <harryxu497@gmail.com>
2024-12-07 16:54:17 -0500

    changed error messages

--- src/lib/models/LedBlock.svelte.ts
+++ src/lib/models/LedBlock.svelte.ts
@@ -18,30 +18,30 @@ class LedBlockModel extends BlockModel<4> {
 
 		// LED Position
 		if (isNaN(ledPosition) || !Number.isInteger(ledPosition)) {
-			throw Error('LED position must be an integer.');
+			throw Error(`LED position must be an integer between 1 and ${programMetadata.numLeds}.`);
 		} else if (ledPosition < 1 || ledPosition > programMetadata.numLeds) {
 			throw Error(`LED position must be an integer between 1 and ${programMetadata.numLeds}.`);
 		}
 
 		// LED Red
 		if (isNaN(ledRed) || !Number.isInteger(ledRed)) {
-			throw Error('LED red value must be an integer.');
+			throw Error('Red value must be an integer between 1 and 255.');
 		} else if (ledRed < 0 || ledRed > 255) {
-			throw Error(`Red value must be an integer between 1 and 255.`);
+			throw Error('Red value must be an integer between 1 and 255.');
 		}
 
 		// LED Green
 		if (isNaN(ledGreen) || !Number.isInteger(ledGreen)) {
-			throw Error(' Green value must be an integer.');
+			throw Error('Green value must be an integer between 1 and 255.');
 		} else if (ledGreen < 0 || ledGreen > 255) {
-			throw Error(`Green value must be an integer between 1 and 255.`);
+			throw Error('Green value must be an integer between 1 and 255.');
 		}
 
 		// LED Blue
 		if (isNaN(ledBlue) || !Number.isInteger(ledBlue)) {
-			throw Error('Blue value must be an integer.');
+			throw Error('Blue value must be an integer between 1 and 255.');
 		} else if (ledBlue < 0 || ledBlue > 255) {
-			throw Error(`Blue value must be an integer between 1 and 255.`);
+			throw Error('Blue value must be an integer between 1 and 255.');
 		}
 
 		return `leds[${ledPosition - 1}] = CRGB(${ledRed}, ${ledGreen}, ${ledBlue});`;
